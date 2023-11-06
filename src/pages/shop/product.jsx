4b3b83eb9c9e7ffe5c1f0aef544dcd2256950e02
AndriAlpian06 <61491000+AndriAlpian06@users.noreply.github.com>
2023-11-06 21:07:33 +0700

    add condition addcart

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ShopContext } from "../../context/shop-context";
 
 
@@ -17,7 +17,7 @@ export const Product = (props) => {
   const cartItemCount = cartItems[id];
 
   // State untuk melacak status login pengguna
-  //const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <>
@@ -29,14 +29,15 @@ export const Product = (props) => {
             }}>
             <button 
                 className="p-2 rounded-full bg-green-500 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500"
-                // onClick={() => {
-                //   if (isUserLoggedIn) {
-                //       // Lakukan logika penambahan ke keranjang
-                //   } else {
-                //       alert("Anda harus login terlebih dahulu untuk menambahkan ke keranjang.");
-                //   }
-                // }}
-                onClick={() => addToCart(id)} 
+                onClick={() => {
+                  if (isLoggedIn) {
+                      // Lakukan logika penambahan ke keranjang
+                       addToCart(id)
+                  } else {
+                      alert("Anda harus login terlebih dahulu untuk menambahkan ke keranjang.");
+                  }
+                }}
+                //onClick={() => addToCart(id)} 
                 >
                 <svg className="h-5 w-5" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
                   <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z">
